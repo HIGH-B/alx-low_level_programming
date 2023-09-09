40a76084db977acdf62869df517edc4e9a3e04e5
High-B <johnsondikee@gmail.com>
2023-09-09 01:23:02 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -  program that prints all possible
+ * combinations of single-digit numbers
+ * Numbers must be separated by ,, followed by a space
+ * Numbers should be printed in ascending order
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
