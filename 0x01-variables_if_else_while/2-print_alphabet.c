5cb27ed9a5e16e9bdaed18b91fd44db2c6d65ac2
High-B <johnsondikee@gmail.com>
2023-09-08 22:46:13 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints the alphabets.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int n = 97;
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+putchar ('\n');
+return (0);
+}
